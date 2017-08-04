d8e532177a6b5cab88b81858fd94489a152103a3
vjcspy <mr.vjcspy@gmail.com>
2017-08-04 22:00:42 +0700

    - merge branch rebase

--- f1.ts
+++ f1.ts
@@ -4,3 +4,9 @@
 // 1
 // ...
 // ...
+// Rebase là như thế nào
+/*
+ *  Đầu tiên git sẽ đi tới cha chung của 2 nhánh.(Nhánh đang làm việc và nhánh muốn rebase)
+ *  Tìm sự khác biệt mà nhánh hiện tại và lưu lại
+ *  Khôi phục lại nhanh hiện tại về nhánh muốn rebase(ở đây là master), đi đến commit cuối cùng ở master và apply lần lượt các thay đổi vào đấy.
+ * */
